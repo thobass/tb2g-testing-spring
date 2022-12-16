d498b106c1a502a1e22fe08cdcbe502d649938a9
BASSET, Thomas <thomas.basset@cgi.com>
2022-12-16 09:18:44 +0100

    Spring Test Properties

---
+++ src/main/java/org/springframework/samples/petclinic/sfg/PropertiesWordProducer.java
@@ -0,0 +1,24 @@
+package org.springframework.samples.petclinic.sfg;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+@Profile("externalized")
+@Primary
+public class PropertiesWordProducer implements WordProducer{
+
+    private String word;
+
+    @Override
+    public String getWord() {
+        return word;
+    }
+
+    @Value("${say.word}")
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
